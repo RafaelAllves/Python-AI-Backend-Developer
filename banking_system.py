22017d430150d06913f659a422deb2bcd39a350f
Rafael Alves <rafaelsralves91@gmail.com>
2024-04-17 21:58:15 -0300

    Create the PessoaFisica class

--- banking_system.py
+++ banking_system.py
@@ -13,6 +13,13 @@ class Cliente():
     def add_conta(self, conta):
         self.contas.append(conta)
 
+class PessoaFisica(Cliente):
+    def __init__(self, cpf, nome, data_nascimento, endereco):
+        super().__init__(endereco)
+        self.cpf = cpf
+        self.nome = nome
+        self.data_nascimento = data_nascimento
+
 
 LIMITE_SAQUES = 3
 
