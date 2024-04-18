60b0df4eabda23b8f89925df60cd89e4f63b3b1b
Rafael Alves <rafaelsralves91@gmail.com>
2024-04-17 23:03:28 -0300

    Create the Deposito class

--- banking_system.py
+++ banking_system.py
@@ -134,6 +134,17 @@ class Saque(Transacao):
         if conta.sacar(self.valor):
             conta.historico.adicionar_transacao(self)
     
+class Deposito(Transacao):
+    def __init__(self, valor):
+        self._valor = valor
+
+    @property
+    def valor(self):
+        return self._valor
+
+    def registrar(self, conta):
+        if conta.depositar(self.valor):
+            conta.historico.adicionar_transacao(self)
 
 LIMITE_SAQUES = 3
 
