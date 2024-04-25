79e9b67f63c6119f8e086676a831c450f0e3fb47
Rafael Alves <rafaelsralves91@gmail.com>
2024-04-25 11:27:34 -0300

    Basics routefor Categoria

---
+++ workout_api/workout_api/categorias/controller.py
@@ -0,0 +1,19 @@
+from fastapi import APIRouter, Body, status
+from workout_api.categorias.schemas import CategoriaIn, CategoriaOut
+from workout_api.contrib.dependencies import DatabaseDependency
+from sqlalchemy.future import select
+
+router = APIRouter()
+
+@router.post(
+    '/', 
+    summary='Criar uma nova Categoria',
+    status_code=status.HTTP_201_CREATED,
+    response_model=CategoriaOut,
+)
+async def post(
+    db_session: DatabaseDependency, 
+    categoria_in: CategoriaIn = Body(...)
+) -> CategoriaOut:
+    pass
+    
