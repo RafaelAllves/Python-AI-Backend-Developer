2b2f556a6e899a5135fff694b70d6d5104c36df7
Rafael Alves <rafaelsralves91@gmail.com>
2024-04-25 11:49:11 -0300

    Create route to query category by id

--- workout_api/workout_api/categorias/controller.py
+++ workout_api/workout_api/categorias/controller.py
@@ -1,5 +1,7 @@
+from http.client import HTTPException
 from uuid import uuid4
 from fastapi import APIRouter, Body, status
+from pydantic import UUID4
 from workout_api.categorias.models import CategoriaModel
 from workout_api.categorias.schemas import CategoriaIn, CategoriaOut
 from workout_api.contrib.dependencies import DatabaseDependency
@@ -25,6 +27,7 @@ async def post(
 
     return categoria_out
 
+
 @router.get(
     '/', 
     summary='Consultar todas as Categorias',
@@ -35,3 +38,23 @@ async def query(db_session: DatabaseDependency) -> list[CategoriaOut]:
     categorias: list[CategoriaOut] = (await db_session.execute(select(CategoriaModel))).scalars().all()
     
     return categorias
+
+
+@router.get(
+    '/{id}', 
+    summary='Consulta uma Categoria pelo id',
+    status_code=status.HTTP_200_OK,
+    response_model=CategoriaOut,
+)
+async def get(id: UUID4, db_session: DatabaseDependency) -> CategoriaOut:
+    categoria: CategoriaOut = (
+        await db_session.execute(select(CategoriaModel).filter_by(id=id))
+    ).scalars().first()
+
+    if not categoria:
+        raise HTTPException(
+            status.HTTP_404_NOT_FOUND, 
+            'Categoria não encontrada'
+        )
+    
+    return categoria
