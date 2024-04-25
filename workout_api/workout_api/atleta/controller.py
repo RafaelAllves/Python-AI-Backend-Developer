d81242b6542617ab2b90eef0c093cb9b5afa767e
Rafael Alves <rafaelsralves91@gmail.com>
2024-04-25 11:17:25 -0300

    Create AtletaIn and AtletaOut

--- workout_api/workout_api/atleta/controller.py
+++ workout_api/workout_api/atleta/controller.py
@@ -1,5 +1,6 @@
-from fastapi import APIRouter, status
-
+from fastapi import APIRouter, Body, status
+from workout_api.atleta.schemas import AtletaIn
+from workout_api.contrib.dependencies import DatabaseDependency
 
 router = APIRouter()
 
@@ -8,5 +9,5 @@ router = APIRouter()
     summary='Criar um novo atleta',
     status_code=status.HTTP_201_CREATED,
 )
-async def post():
+async def post(db_session: DatabaseDependency, atleta_in: AtletaIn = Body(...)):
    pass
