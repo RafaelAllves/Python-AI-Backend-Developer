b819e8cadcd941938fbbf2cddbb29e0ccf3fdebe
Rafael Alves <rafaelsralves91@gmail.com>
2024-04-26 19:26:19 -0300

    Create route to create a new Training Center

---
+++ workout_api/workout_api/centro_treinamento/controller.py
@@ -0,0 +1,27 @@
+from uuid import uuid4
+from fastapi import APIRouter, Body, status
+from pydantic import UUID4
+from workout_api.centro_treinamento.schemas import CentroTreinamentoIn, CentroTreinamentoOut
+from workout_api.centro_treinamento.models import CentroTreinamentoModel
+from workout_api.contrib.dependencies import DatabaseDependency
+from sqlalchemy.future import select
+
+router = APIRouter()
+
+@router.post(
+    '/', 
+    summary='Criar um novo Centro de treinamento',
+    status_code=status.HTTP_201_CREATED,
+    response_model=CentroTreinamentoOut,
+)
+async def post(
+    db_session: DatabaseDependency, 
+    centro_treinamento_in: CentroTreinamentoIn = Body(...)
+) -> CentroTreinamentoOut:
+    centro_treinamento_out = CentroTreinamentoOut(id=uuid4(), **centro_treinamento_in.model_dump())
+    centro_treinamento_model = CentroTreinamentoModel(**centro_treinamento_out.model_dump())
+    
+    db_session.add(centro_treinamento_model)
+    await db_session.commit()
+
+    return centro_treinamento_out
