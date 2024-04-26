dd5d42641021241f26efd4620e910eb331fbcce4
Rafael Alves <rafaelsralves91@gmail.com>
2024-04-26 20:02:28 -0300

    Create route to insert new athlete

--- workout_api/workout_api/atleta/controller.py
+++ workout_api/workout_api/atleta/controller.py
@@ -1,5 +1,11 @@
+from datetime import datetime
 from fastapi import APIRouter, Body, status
-from workout_api.atleta.schemas import AtletaIn
+from sqlalchemy.future import select
+from uuid import uuid4
+from workout_api.atleta.schemas import AtletaIn, AtletaOut, AtletaUpdate
+from workout_api.atleta.models import AtletaModel
+from workout_api.categorias.models import CategoriaModel
+from workout_api.centro_treinamento.models import CentroTreinamentoModel
 from workout_api.contrib.dependencies import DatabaseDependency
 
 router = APIRouter()
@@ -8,6 +14,30 @@ router = APIRouter()
     '/', 
     summary='Criar um novo atleta',
     status_code=status.HTTP_201_CREATED,
+    response_model=AtletaOut
 )
-async def post(db_session: DatabaseDependency, atleta_in: AtletaIn = Body(...)):
-   pass
+async def post(
+    db_session: DatabaseDependency, 
+    atleta_in: AtletaIn = Body(...)
+):
+    categoria_nome = atleta_in.categoria.nome
+    centro_treinamento_nome = atleta_in.centro_treinamento.nome
+
+    categoria = (await db_session.execute(
+        select(CategoriaModel).filter_by(nome=categoria_nome))
+    ).scalars().first()
+    
+    centro_treinamento = (await db_session.execute(
+        select(CentroTreinamentoModel).filter_by(nome=centro_treinamento_nome))
+    ).scalars().first()
+    
+    atleta_out = AtletaOut(id=uuid4(), created_at=datetime.now(datetime.timezone.utc), **atleta_in.model_dump())
+    atleta_model = AtletaModel(**atleta_out.model_dump(exclude={'categoria', 'centro_treinamento'}))
+
+    atleta_model.categoria_id = categoria.pk_id
+    atleta_model.centro_treinamento_id = centro_treinamento.pk_id
+    
+    db_session.add(atleta_model)
+    await db_session.commit()
+
+    return atleta_out
