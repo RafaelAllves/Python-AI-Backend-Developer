15f36ef7e885a9a6127555b445e2d8ac46f5ee98
Rafael Alves <rafaelsralves91@gmail.com>
2024-04-26 20:05:29 -0300

    Handle exceptions

--- workout_api/workout_api/atleta/controller.py
+++ workout_api/workout_api/atleta/controller.py
@@ -1,8 +1,8 @@
 from datetime import datetime
-from fastapi import APIRouter, Body, status
+from fastapi import APIRouter, Body, status, HTTPException
 from sqlalchemy.future import select
 from uuid import uuid4
-from workout_api.atleta.schemas import AtletaIn, AtletaOut, AtletaUpdate
+from workout_api.atleta.schemas import AtletaIn, AtletaOut
 from workout_api.atleta.models import AtletaModel
 from workout_api.categorias.models import CategoriaModel
 from workout_api.centro_treinamento.models import CentroTreinamentoModel
@@ -27,17 +27,34 @@ async def post(
         select(CategoriaModel).filter_by(nome=categoria_nome))
     ).scalars().first()
     
+    if not categoria:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, 
+            detail=f'A categoria {categoria_nome} não foi encontrada.'
+        )
+    
     centro_treinamento = (await db_session.execute(
         select(CentroTreinamentoModel).filter_by(nome=centro_treinamento_nome))
     ).scalars().first()
     
-    atleta_out = AtletaOut(id=uuid4(), created_at=datetime.now(datetime.timezone.utc), **atleta_in.model_dump())
-    atleta_model = AtletaModel(**atleta_out.model_dump(exclude={'categoria', 'centro_treinamento'}))
+    if not centro_treinamento:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, 
+            detail=f'O centro de treinamento {centro_treinamento_nome} não foi encontrado.'
+        )
+    try:
+        atleta_out = AtletaOut(id=uuid4(), created_at=datetime.now(datetime.timezone.utc), **atleta_in.model_dump())
+        atleta_model = AtletaModel(**atleta_out.model_dump(exclude={'categoria', 'centro_treinamento'}))
 
-    atleta_model.categoria_id = categoria.pk_id
-    atleta_model.centro_treinamento_id = centro_treinamento.pk_id
-    
-    db_session.add(atleta_model)
-    await db_session.commit()
+        atleta_model.categoria_id = categoria.pk_id
+        atleta_model.centro_treinamento_id = centro_treinamento.pk_id
+        
+        db_session.add(atleta_model)
+        await db_session.commit()
+    except Exception:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, 
+            detail='Ocorreu um erro ao inserir os dados no banco'
+        )
 
     return atleta_out
