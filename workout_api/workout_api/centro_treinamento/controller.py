417600eb1979a57de73754b62268613d38f14bed
Rafael Alves <rafaelsralves91@gmail.com>
2024-04-27 09:27:41 -0300

    Add pagination to training center query

--- workout_api/workout_api/centro_treinamento/controller.py
+++ workout_api/workout_api/centro_treinamento/controller.py
@@ -1,11 +1,12 @@
 from uuid import uuid4
 from fastapi import APIRouter, Body, HTTPException, status
+from fastapi_pagination import Page, paginate
 from pydantic import UUID4
+from sqlalchemy.future import select
+from sqlalchemy.exc import IntegrityError
 from workout_api.centro_treinamento.schemas import CentroTreinamentoIn, CentroTreinamentoOut
 from workout_api.centro_treinamento.models import CentroTreinamentoModel
 from workout_api.contrib.dependencies import DatabaseDependency
-from sqlalchemy.future import select
-from sqlalchemy.exc import IntegrityError
 
 router = APIRouter()
 
@@ -46,14 +47,14 @@ async def post(
     '/', 
     summary='Consultar todos os centros de treinamento',
     status_code=status.HTTP_200_OK,
-    response_model=list[CentroTreinamentoOut],
+    response_model=Page[CentroTreinamentoOut],
 )
-async def query(db_session: DatabaseDependency) -> list[CentroTreinamentoOut]:
+async def query(db_session: DatabaseDependency) -> Page[CentroTreinamentoOut]:
     centros_treinamento_out: list[CentroTreinamentoOut] = (
         await db_session.execute(select(CentroTreinamentoModel))
     ).scalars().all()
     
-    return centros_treinamento_out
+    return paginate(centros_treinamento_out)
 
 
 @router.get(
