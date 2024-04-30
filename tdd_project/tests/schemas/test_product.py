dc5ed8029d6835d64fbce4df237c9edc4364c6a5
Rafael Alves <rafaelsralves91@gmail.com>
2024-04-30 09:34:44 -0300

    Fix test_schemas_validated

--- tdd_project/tests/schemas/test_product.py
+++ tdd_project/tests/schemas/test_product.py
@@ -2,8 +2,8 @@ from store.schemas.product import ProductIn
 
 
 def test_schemas_validated():
-    data = {"name": "Iphone 14 Pro Max", "quantity": 10, "price": 8.500}
+    data = {"name": "Iphone 14 Pro Max", "quantity": 10, "price": 8.500, "status": True}
 
-    product = ProductIn(data)
+    product = ProductIn(**data)
 
     assert product.name == "Iphone 14 Pro Max"
