81635491351e7b10e24a79d86ead86d1ec968712
Rafael Alves <rafaelsralves91@gmail.com>
2024-04-30 09:33:18 -0300

    Create schema product

---
+++ tdd_project/store/schemas/product.py
@@ -0,0 +1,14 @@
+from decimal import Decimal
+from pydantic import Field
+from store.schemas.base import BaseSchemaMixin
+
+
+class ProductBase(BaseSchemaMixin):
+    name: str = Field(..., description="Product name")
+    quantity: int = Field(..., description="Product quantity")
+    price: Decimal = Field(..., description="Product price")
+    status: bool = Field(..., description="Product status")
+
+
+class ProductIn(ProductBase, BaseSchemaMixin):
+    ...
