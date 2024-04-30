dbbf97470f43237b6bb2fe3190ddb3ee984832da
Rafael Alves <rafaelsralves91@gmail.com>
2024-04-30 09:11:39 -0300

    Factory products_data

--- tdd_project/tests/factories.py
+++ tdd_project/tests/factories.py
@@ -5,3 +5,17 @@ def product_data():
         "price": "8.500",
         "status": True,
     }
+
+
+def products_data():
+    return [
+        {"name": "Iphone 11 Pro Max", "quantity": 20, "price": "4.500", "status": True},
+        {"name": "Iphone 12 Pro Max", "quantity": 15, "price": "5.500", "status": True},
+        {"name": "Iphone 13 Pro Max", "quantity": 5, "price": "6.500", "status": True},
+        {
+            "name": "Iphone 15 Pro Max",
+            "quantity": 3,
+            "price": "10.500",
+            "status": False,
+        },
+    ]
