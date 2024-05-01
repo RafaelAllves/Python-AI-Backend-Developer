5cb50dbdab8d5bb70e50324d9e69b281298b2743
Rafael Alves <rafaelsralves91@gmail.com>
2024-05-01 18:18:32 -0300

    Create ProductUsecase get

--- tdd_project/store/usecases/product.py
+++ tdd_project/store/usecases/product.py
@@ -1,3 +1,4 @@
+from uuid import UUID
 from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorDatabase
 from store.db.mongo import db_client
 from store.schemas.product import ProductIn, ProductOut
@@ -12,6 +13,11 @@ class ProductUsecase:
     async def create(self, body: ProductIn) -> ProductOut:
         product = await self.collection.insert_one(body.model_dump())
         return product
+
+    async def get(self, id: UUID) -> ProductOut:
+        result = await self.collection.find_one({"id": id})
+
+        return ProductOut(**result)
 
 
 product_usecase = ProductUsecase()
