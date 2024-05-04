4c480c80b99d62ff1de30dea314c66936e9284c2
Rafael Alves <rafaelsralves91@gmail.com>
2024-05-04 14:50:53 -0300

    Create controller get product

--- tdd_project/store/controllers/product.py
+++ tdd_project/store/controllers/product.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter, Body, Depends, status
+from fastapi import APIRouter, Body, Depends, status, Path
+from pydantic import UUID4
 from store.schemas.product import ProductIn, ProductOut
 from store.usecases.product import ProductUsecase
 
@@ -10,3 +11,10 @@ async def post(
     body: ProductIn = Body(...), usecase: ProductUsecase = Depends()
 ) -> ProductOut:
     return await usecase.create(body=body)
+
+
+@router.get(path="/{id}", status_code=status.HTTP_200_OK)
+async def get(
+    id: UUID4 = Path(alias="id"), usecase: ProductUsecase = Depends()
+) -> ProductOut:
+    return await usecase.get(id=id)
