53a9e2630cf66e6a921a7f12371c615533f1681f
Rafael Alves <rafaelsralves91@gmail.com>
2024-05-04 09:49:15 -0300

    Fixs types

--- tdd_project/tests/factories.py
+++ tdd_project/tests/factories.py
@@ -12,25 +12,25 @@ def products_data():
         {
             "name": "Iphone 11 Pro Max",
             "quantity": 20,
-            "price": float("4.500"),
+            "price": "4.500",
             "status": True,
         },
         {
             "name": "Iphone 12 Pro Max",
             "quantity": 15,
-            "price": float("5.500"),
+            "price": "5.500",
             "status": True,
         },
         {
             "name": "Iphone 13 Pro Max",
             "quantity": 5,
-            "price": float("6.500"),
+            "price": "6.500",
             "status": True,
         },
         {
             "name": "Iphone 15 Pro Max",
             "quantity": 3,
-            "price": float("10.500"),
+            "price": "10.500",
             "status": False,
         },
     ]
