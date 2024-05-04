47c40ff54c2876e4e2761ab4084cff4a3d21258c
Rafael Alves <rafaelsralves91@gmail.com>
2024-05-04 08:55:43 -0300

    Test test_usecases_delete_should_not_found

--- tdd_project/tests/usecases/test_product.py
+++ tdd_project/tests/usecases/test_product.py
@@ -57,3 +57,14 @@ async def test_usecases_delete_should_return_success(product_inserted):
     result = await product_usecase.delete(id=str(product.id))
 
     assert result is True
+
+
+@pytest.mark.asyncio
+async def test_usecases_delete_should_not_found():
+    with pytest.raises(NotFoundException) as err:
+        await product_usecase.delete(id=UUID("1e4f214e-85f7-461a-89d0-a751a32e3bb9"))
+
+    assert (
+        err.value.message
+        == "Product not found with filter: 1e4f214e-85f7-461a-89d0-a751a32e3bb9"
+    )
