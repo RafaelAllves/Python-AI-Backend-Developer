0aa89f2c416bf2bceb450eb9c2e404a85bde757c
Rafael Alves <rafaelsralves91@gmail.com>
2024-05-04 14:57:08 -0300

    Test test_controller_query_should_return_success

--- tdd_project/tests/controllers/test_product.py
+++ tdd_project/tests/controllers/test_product.py
@@ -1,3 +1,4 @@
+from typing import List
 import pytest
 from tests.factories import product_data
 from fastapi import status
@@ -51,3 +52,13 @@ async def test_controller_get_should_return_not_found(client, products_url):
     assert response.json() == {
         "detail": "Product not found with filter: 4fd7cd35-a3a0-4c1f-a78d-d24aa81e7dca"
     }
+
+
+@pytest.mark.asyncio
+@pytest.mark.usefixtures("products_inserted")
+async def test_controller_query_should_return_success(client, products_url):
+    response = await client.get(products_url)
+
+    assert response.status_code == status.HTTP_200_OK
+    assert isinstance(response.json(), List)
+    assert len(response.json()) > 1
