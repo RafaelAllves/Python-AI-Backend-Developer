79d0107284a2ddd51c6d897851345a91a601d283
Rafael Alves <rafaelsralves91@gmail.com>
2024-05-04 18:20:28 -0300

    Handle error in create product

--- tdd_project/store/controllers/product.py
+++ tdd_project/store/controllers/product.py
@@ -12,7 +12,10 @@ router = APIRouter(tags=["products"])
 async def post(
     body: ProductIn = Body(...), usecase: ProductUsecase = Depends()
 ) -> ProductOut:
-    return await usecase.create(body=body)
+    try:
+        return await usecase.create(body=body)
+    except Exception as exc:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(exc))
 
 
 @router.get(path="/{id}", status_code=status.HTTP_200_OK)
