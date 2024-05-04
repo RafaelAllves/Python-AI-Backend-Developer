b3db7354cad680d65a10b3bb4c2ba0c0aeef53a0
Rafael Alves <rafaelsralves91@gmail.com>
2024-05-04 11:32:55 -0300

    Test test_controller_create_should_return_success

---
+++ tdd_project/tests/controllers/test_product.py
@@ -0,0 +1,22 @@
+import pytest
+from tests.factories import product_data
+from fastapi import status
+
+
+@pytest.mark.asyncio
+async def test_controller_create_should_return_success(client, products_url):
+    response = await client.post(products_url, json=product_data())
+
+    content = response.json()
+
+    del content["created_at"]
+    del content["updated_at"]
+    del content["id"]
+
+    assert response.status_code == status.HTTP_201_CREATED
+    assert content == {
+        "name": "Iphone 14 Pro Max",
+        "quantity": 10,
+        "price": "8.500",
+        "status": True,
+    }
